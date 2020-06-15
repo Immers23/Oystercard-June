c9117a26f18ce7ac9d3f0f078e88aefcc2623ea8
Imraan Ali <imraan91@hotmail.co.uk>
2020-06-15 13:13:04 +0100

    touch_in and touch_out methods added with predicate method of in_journey?

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -2,13 +2,34 @@ class Oystercard
 
   attr_reader :balance
 
+  MAXIMUM_BALANCE = 90
+
   def initialize
     @balance = 0
+    @in_journey = false
   end
 
   def top_up(amount)
+    raise "Maximum balance of #{MAXIMUM_BALANCE} exceeded" if amount + balance > MAXIMUM_BALANCE
     @balance += amount
   end
+
+  def deduct(amount)
+    @balance -= amount
+  end
+
+  def touch_in
+    @in_journey = true
+  end
+
+  def touch_out
+    @in_journey = false
+  end
+
+  def in_journey?
+    @in_journey
+  end
+
 
 
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -8,10 +8,48 @@ describe Oystercard do
 
   describe '#top_up' do
     it { is_expected.to respond_to(:top_up).with(1).argument }
+
+    it 'can top up the balance' do
+      expect{ subject.top_up 1 }.to change{ subject.balance }.by 1
+    end
+
   end
 
-  it 'can top up the balance' do
-    expect{ subject.top_up 1 }.to change{ subject.balance }.by 1
+  describe '#deduct' do
+    it { is_expected.to respond_to(:deduct).with(1).argument }
+
+    it 'deducts an amount from the balance' do
+      subject.top_up(20)
+      expect{ subject.deduct 3}.to change{ subject.balance }.by -3
+    end
+  end
+
+  it 'is initially not in a journey' do
+    expect(subject).not_to be_in_journey
+  end
+
+  describe 'touch in and touch out' do
+
+    before(:each) do
+      subject.top_up(Oystercard::MAXIMUM_BALANCE)
+    end
+
+    it "can touch in" do
+      subject.touch_in
+      expect(subject).to be_in_journey
+    end
+
+    it "can touch out" do
+      subject.touch_in
+      subject.touch_out
+      expect(subject).not_to be_in_journey
+    end
+  end
+
+  it 'raises an error if the maximum balance is exceeded' do
+    maximum_balance = Oystercard::MAXIMUM_BALANCE
+    subject.top_up(maximum_balance)
+    expect{ subject.top_up 1 }.to raise_error "Maximum balance of #{maximum_balance} exceeded"
   end
 
 end
