37d9093f284969dfa75d0261a29828f29dc80dd6
Imraan Ali <imraan91@hotmail.co.uk>
2020-07-11 14:17:16 +0100

    entry station is now being recorded

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,6 +1,6 @@
 class Oystercard
 
-  attr_reader :balance
+  attr_reader :balance, :entry_station
 
   MAXIMUM_BALANCE = 90
   MINIMUM_CHARGE = 1
@@ -9,6 +9,7 @@ class Oystercard
   def initialize
     @balance = 0
     @in_journey = false
+    @entry_station = nil
   end
 
   def top_up(amount)
@@ -16,18 +17,19 @@ class Oystercard
     @balance += amount
   end
 
-  def touch_in
+  def touch_in(station)
     raise "Insufficient balance to touch in" if balance < MINIMUM_BALANCE
+    @entry_station = station
     @in_journey = true
   end
 
   def touch_out
     deduct(MINIMUM_CHARGE)
-    @in_journey = false
+    @entry_station = nil
   end
 
   def in_journey?
-    @in_journey
+    !!entry_station
   end
 
   private
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -2,6 +2,8 @@ require 'oystercard'
 
 describe Oystercard do
 
+  let(:station){ double :station }
+
   it 'has a balance of 0' do
     expect(subject.balance).to eq (0)
   end
@@ -26,24 +28,30 @@ describe Oystercard do
     end
 
     it "can touch in" do
-      subject.touch_in
+      subject.touch_in(station)
       expect(subject).to be_in_journey
     end
 
     it "can touch out" do
-      subject.touch_in
+      subject.touch_in(station)
       subject.touch_out
       expect(subject).not_to be_in_journey
     end
 
     it 'will deduct the minimum balance on touch out' do
-      subject.touch_in
+      subject.touch_in(station)
       expect{ subject.touch_out }.to change{ subject.balance }.by(-Oystercard::MINIMUM_CHARGE)
     end
+
+    it 'stores the entry station' do
+      subject.touch_in(station)
+      expect(subject.entry_station).to eq station
+    end
+
   end
 
   it 'will not touch in if below minimum balance' do
-    expect{ subject.touch_in }.to raise_error "Insufficient balance to touch in"
+    expect{ subject.touch_in(station) }.to raise_error "Insufficient balance to touch in"
   end
 
   it 'raises an error if the maximum balance is exceeded' do
